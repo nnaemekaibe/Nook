90db1b345805e064e238d8d2e9411eee49b65592
Bryan Ibe <nnaemekaibe@users.noreply.github.com>
2022-10-14 14:57:57 +0100

    use api_base_url from env file

--- src/repository/notes.respository.ts
+++ src/repository/notes.respository.ts
@@ -4,11 +4,14 @@ const options = {
         },
     }
 export const getPageBlocks = async (id: string) =>{
-    const res = await fetch(`https://wcohsn-3000.preview.csb.app/notion/${id}`, options)
+    const res = await fetch(`${process.env.REACT_APP_API_BASE}/notion/${id}`, options)
     return res.json()
 }
 
 export const getEverythingAvailableToBot = async () =>{
-    const res = await fetch("https://wcohsn-3000.preview.csb.app/search", options)
+    const res = await fetch(
+      `${process.env.REACT_APP_API_BASE}/search`,
+      options
+    );
     return res.json()
 }
